041421ae37259f13f8e09a2d49b9bc9cf65baff4
alisa <alisa@alisaqaq.moe>
2023-11-24 09:31:19 +0800

    fix: option could not be built

--- src/HyperDimension.Infrastructure.Cache/Options/Providers/MemoryOptions.cs
+++ src/HyperDimension.Infrastructure.Cache/Options/Providers/MemoryOptions.cs
@@ -1,5 +1,8 @@
-﻿namespace HyperDimension.Infrastructure.Cache.Options.Providers;
+﻿using HyperDimension.Common.Attributes;
 
+namespace HyperDimension.Infrastructure.Cache.Options.Providers;
+
+[OptionSection("Cache:Memory")]
 public class MemoryOptions
 {
     public int SizeLimit { get; set; } = 1024;
